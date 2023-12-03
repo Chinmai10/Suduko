2ccd29a3112e46589d1711094d3de10150d6cd20
Chinmai10 <srichinmai2004@gmail.com>
2023-12-03 22:54:33 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+WIDTH = 600
+background_color =  (245,200,200)
+
+def main():
+    pygame.init()
+    win = pygame.display.set_mode((WIDTH , WIDTH))
+    pygame.display.set_caption("SUDOKU")
+    
+    
+    while True :
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT :
+                pygame.quit()
+                return 
+    
+    
+main()
