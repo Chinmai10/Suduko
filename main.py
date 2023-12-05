aec9a7fa1ee80660b8450efee914521822abf618
Chinmai10 <srichinmai2004@gmail.com>
2023-12-06 00:20:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import pygame
 
-WIDTH = 600
-background_color =  (245,205,200)
+WIDTH =550
+background_color =  (251,247,245)
 
 def main():
     pygame.init()
     win = pygame.display.set_mode((WIDTH , WIDTH))
     pygame.display.set_caption("SUDOKU")
+    win.fill(background_color)
+    
+    for i in range(0 ,10) :
+        if(i%3 ==0 ):
+            pygame.draw.line(win , (0,0,0))
+    pygame.draw.line(win , (0,0 ,0) , (50+ 50*i , 50  ) , (50 + 50*i ,500),2)
+    pygame.draw.line(win , (0,0 ,0) , ( 50 , 50+ 50*i  ) , (500  ,50 + 50*i),2)
+    pygame.display.update()  
     
     
     while True :
