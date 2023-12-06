93409b5c40ee529d09b1dc0ddd7f363226f54ae5
Chinmai10 <srichinmai2004@gmail.com>
2023-12-06 08:50:07 +0530

    third commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import 
+
 
 WIDTH =550
 background_color =  (251,247,245)
@@ -7,19 +9,24 @@ def main():
     pygame.init()
     win = pygame.display.set_mode((WIDTH , WIDTH))
     pygame.display.set_caption("SUDOKU")
+    
     win.fill(background_color)
     
     for i in range(0 ,10) :
         if(i%3 ==0 ):
-            pygame.draw.line(win , (0,0,0))
-    pygame.draw.line(win , (0,0 ,0) , (50+ 50*i , 50  ) , (50 + 50*i ,500),2)
-    pygame.draw.line(win , (0,0 ,0) , ( 50 , 50+ 50*i  ) , (500  ,50 + 50*i),2)
+            pygame.draw.line(win , (0,0 ,0) , (50+ 50*i , 50  ) , (50 + 50*i ,500),6)
+            pygame.draw.line(win , (0,0 ,0) , ( 50 , 50+ 50*i  ) , (500  ,50 + 50*i),6)
+             
+            
+            
+        pygame.draw.line(win , (0,0 ,0) , (50+ 50*i , 50  ) , (50 + 50*i ,500),2)
+        pygame.draw.line(win , (0,0 ,0) , ( 50 , 50+ 50*i  ) , (500  ,50 + 50*i),2)
     pygame.display.update()  
     
     
     while True :
         for event in pygame.event.get():
-            if event.type == pygame.QUIT :
+            if event.type == pygame.QUIT : suduko.py
                 pygame.quit()
                 return 
     
